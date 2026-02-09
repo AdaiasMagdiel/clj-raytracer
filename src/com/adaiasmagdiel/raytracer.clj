3b1ced735dedfff8e38a53367170548013ad1b2e
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-09 11:23:40 -0300

    style: fix indentation of main function

--- src/com/adaiasmagdiel/raytracer.clj
+++ src/com/adaiasmagdiel/raytracer.clj
@@ -1,11 +1,11 @@
 (ns com.adaiasmagdiel.raytracer
-	(:gen-class)
-	(:require [com.adaiasmagdiel.quill :as q]
-											[com.adaiasmagdiel.image :as img]))
+  (:gen-class)
+  (:require [com.adaiasmagdiel.quill :as q]
+            [com.adaiasmagdiel.image :as img]))
 
 (defn -main
-	"Main entry point for the raytracer application."
-	[& args]
-	(if (= (first args) "--image")
-		(img/main)
-		(q/main)))
+  "Main entry point for the raytracer application."
+  [& args]
+  (if (= (first args) "--image")
+    (img/main)
+    (q/main)))
