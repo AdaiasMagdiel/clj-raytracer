72154b1d96dbcf918c059a049d3874cfeee31507
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-09 12:39:04 -0300

    feat: Add sphere hit implementation
    - Add com.adaiasmagdiel.raytracer.sphere namespace with create and hit functions
    - Implements intersection logic for rays with spheres
    - Uses math and vector utilities for calculations

---
+++ src/com/adaiasmagdiel/raytracer/sphere.clj
@@ -0,0 +1,43 @@
+(ns com.adaiasmagdiel.raytracer.sphere
+  (:require [com.adaiasmagdiel.raytracer.hittable :as h]
+            [com.adaiasmagdiel.raytracer.vec3 :as vec3]
+            [com.adaiasmagdiel.raytracer.ray :as ray]
+            [clojure.math :as math]))
+
+(defn create [center radius]
+  {:center center
+   :radius radius})
+
+(defn hit
+  [sphere ray tmin tmax]
+  (let [center (:center sphere)
+        radius (:radius sphere)
+        origin (:origin ray)
+        direction (:direction ray)
+
+        oc (vec3/sub center origin)
+
+        a (vec3/length-sq direction)
+        h (vec3/dot direction oc)
+        c (- (vec3/length-sq oc) (* radius radius))
+
+        discriminant (- (* h h) (* a c))]
+
+    (when (>= discriminant 0)
+      (let [sqrtd (math/sqrt discriminant)
+
+            root1 (/ (- h sqrtd) a)
+            root2 (/ (+ h sqrtd) a)
+
+            t (cond
+                (and (> root1 tmin) (< root1 tmax)) root1
+                (and (> root2 tmin) (< root2 tmax)) root2
+                :else nil)]
+
+        (when t
+          (let [p (ray/at ray t)
+                outward-normal (vec3/scalar-div
+                                (vec3/sub p center)
+                                radius)
+                hr (h/hit-record t p outward-normal false)]
+            (h/set-face-normal hr ray outward-normal)))))))
