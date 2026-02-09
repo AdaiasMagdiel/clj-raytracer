1657b5cf56d0405f44556086bd42836f412670e5
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-08 23:55:54 -0300

    refactor: decouple main entry point and add CLI argument support
    - Remove hardcoded Quil logic from the main namespace.
    - Implement a dispatcher in -main to choose between GUI and CLI modes.
    - Add support for the '--image' flag to trigger PPM output via image namespace.
    - Delegate rendering responsibilities to specialized quill and image modules.

--- src/com/adaiasmagdiel/raytracer.clj
+++ src/com/adaiasmagdiel/raytracer.clj
@@ -1,31 +1,11 @@
 (ns com.adaiasmagdiel.raytracer
 	(:gen-class)
-	(:require [quil.core :as q]))
-
-(def WIDTH 640)
-(def HEIGHT 480)
-
-(defn setup []
-	(let [img (q/create-image WIDTH HEIGHT :rgb)]
-		
-		(dotimes [y HEIGHT]
-			(dotimes [x WIDTH]
-				(let [rr (double (/ x (- WIDTH 1)))
-										gg (double (/ y (- HEIGHT 1)))
-										bb 0
-
-										r (int (* 255.999 rr))
-										g (int (* 255.999 gg))
-										b (int (* 255.999 bb))]
-					
-					(q/set-pixel img x y (q/color r g b)))))
-
-		(q/image img 0 0)))
+	(:require [com.adaiasmagdiel.quill :as q]
+											[com.adaiasmagdiel.image :as img]))
 
 (defn -main
+	"Main entry point for the raytracer application."
 	[& args]
-	(q/defsketch raytracer
-  :title "Raytracer"
-  :settings #(q/smooth 2)
-  :setup setup
-  :size [WIDTH HEIGHT]))
+	(if (= (first args) "--image")
+		(img/main)
+		(q/main)))
