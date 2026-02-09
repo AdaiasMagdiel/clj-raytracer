77fd122a69234e3a57986f581ee764794f6f7a55
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-09 00:57:31 -0300

    feat: generate first successful raytraced sky gradient
    - Implement viewport and camera coordinate systems in the engine
    - Apply a linear interpolation (LERP) for the background sky color
    - Normalize color output to 0-255 integers using map and scalar multiplication
    - Successfully export the 'first light' gradient to image.ppm

--- src/com/adaiasmagdiel/engine.clj
+++ src/com/adaiasmagdiel/engine.clj
@@ -1,13 +1,36 @@
 (ns com.adaiasmagdiel.engine
-	(:require [com.adaiasmagdiel.settings :as s]))
+	(:require [com.adaiasmagdiel.settings :as s]
+											[com.adaiasmagdiel.raytracer.vec3 :as vec3]
+											[com.adaiasmagdiel.raytracer.ray :as ray]))
+
+(def focal-length 1.0)
+(def camera-center (vec3/create 0 0 0))
+
+(def viewport-u (vec3/create s/VIEWPORT_WIDTH 0 0))
+(def viewport-v (vec3/create 0 (* -1 s/VIEWPORT_HEIGHT) 0))
+
+(def pixel-delta-u (vec3/scalar-div viewport-u s/WIDTH))
+(def pixel-delta-v (vec3/scalar-div viewport-v s/HEIGHT))
+
+(def viewport-upper-left (vec3/sub
+																										(vec3/sub
+																											(vec3/sub camera-center (vec3/create 0 0 focal-length))
+																											(vec3/scalar-div viewport-u 2))
+																										(vec3/scalar-div viewport-v 2)))
+(def pixel00-loc (vec3/add viewport-upper-left (vec3/scalar-mul (vec3/add pixel-delta-u pixel-delta-v) 0.5)))
+
+(defn ray-color [r]
+	(let [unity-direction (vec3/unit (:direction r))
+							a (* 0.5 (+ 1.0 (vec3/y unity-direction)))]
+			(vec3/add 
+				(vec3/scalar-mul (vec3/create 1.0 1.0 1.0) (- 1.0 a))
+				(vec3/scalar-mul (vec3/create 0.5 0.7 1.0) a))))
 
 (defn compute-pixel-color [x y]
-	(let [rr (double (/ x (- s/WIDTH 1)))
-							gg (double (/ y (- s/HEIGHT 1)))
-							bb 0
+	(let [pixel-center
+							 (vec3/add (vec3/scalar-mul pixel-delta-v y)
+																	(vec3/add pixel00-loc (vec3/scalar-mul pixel-delta-u x)))
+							ray-direction (vec3/sub pixel-center camera-center)
+							r (ray/create camera-center ray-direction)]
 
-							r (int (* 255.999 rr))
-							g (int (* 255.999 gg))
-							b (int (* 255.999 bb))]
-
-		(vector r g b)))
+		(map #(int (* 255.999 %)) (ray-color r))))
