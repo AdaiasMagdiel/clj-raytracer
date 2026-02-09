ec7226f9077647ad98242fcfbc108f0308c84e74
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-09 12:59:26 -0300

    feat: add hit-world and adjust hit-record signature - Provide hit-world function to test ray against all objects in the world - Adjust hit-record signature from [p normal t front-face] - Use new signature [t p normal front-face] for consistency

--- src/com/adaiasmagdiel/raytracer/hittable.clj
+++ src/com/adaiasmagdiel/raytracer/hittable.clj
@@ -2,7 +2,7 @@
   (:require
     [com.adaiasmagdiel.raytracer.vec3 :as vec3]))
 
-(defn hit-record [p normal t front-face]
+(defn hit-record [t p normal front-face]
   {:t t
    :point p
    :normal normal
@@ -12,3 +12,16 @@
   (let [front-face (< (vec3/dot (:direction ray) outward-normal) 0)
         normal (if front-face outward-normal (vec3/scalar-mul outward-normal -1))]
     (hit-record (:t hr) (:point hr) normal front-face)))
+
+(defn hit-world
+  [world ray tmin tmax]
+  (loop [objects world
+         closest tmax
+         hit-record nil]
+    (if (empty? objects)
+      hit-record
+      (let [obj (first objects)
+            hit ((:hit obj) obj ray tmin closest)]
+        (if hit
+          (recur (rest objects) (:t hit) hit)
+          (recur (rest objects) closest hit-record))))))
