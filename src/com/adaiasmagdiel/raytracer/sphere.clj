4c9cd4b8f1785bd60d88723d8f81d76ee5c6bf18
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-14 13:57:46 -0300

    refactor: implement Sphere using deftype for performance optimization - introduce type hints for numerical computations - replace map-based implementation with a typed record - maintain existing functionality and behavior - update create function to return the new Sphere type

--- src/com/adaiasmagdiel/raytracer/sphere.clj
+++ src/com/adaiasmagdiel/raytracer/sphere.clj
@@ -2,43 +2,52 @@
   (:require [com.adaiasmagdiel.raytracer.hittable :as h]
             [com.adaiasmagdiel.raytracer.vec3 :as vec3]
             [com.adaiasmagdiel.raytracer.ray :as ray]
-            [clojure.math :as math]))
+            [clojure.math :as math])
+  (:import [com.adaiasmagdiel.raytracer.ray Ray]
+           [com.adaiasmagdiel.raytracer.vec3 Vec3]
+           [com.adaiasmagdiel.raytracer.hittable HitRecord IHittable]))
 
-(defn hit
-  [sphere ray tmin tmax]
-  (let [center (:center sphere)
-        radius (:radius sphere)
-        origin (:origin ray)
-        direction (:direction ray)
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
 
-        oc (vec3/sub center origin)
+(deftype Sphere
+  [^Vec3 center
+   ^double radius
+   material]
 
-        a (vec3/length-sq direction)
-        h (vec3/dot direction oc)
-        c (- (vec3/length-sq oc) (* radius radius))
+  IHittable
 
-        discriminant (- (* h h) (* a c))]
+  (hit [this ray tmin tmax]
+       (let [^Ray ray ray
+             ^double tmin tmin
+             ^double tmax tmax
+             ^Vec3 origin (.origin ray)
+             ^Vec3 direction (.direction ray)
 
-    (when (>= discriminant 0)
-      (let [sqrtd (math/sqrt discriminant)
+             oc (vec3/sub center origin)
 
-            root1 (/ (- h sqrtd) a)
-            root2 (/ (+ h sqrtd) a)
+             a (vec3/length-sq direction)
+             h (vec3/dot direction oc)
+             c (- (vec3/length-sq oc) (* radius radius))
 
-            t (cond
-                (and (> root1 tmin) (< root1 tmax)) root1
-                (and (> root2 tmin) (< root2 tmax)) root2
-                :else nil)]
+             discriminant (- (* h h) (* a c))]
 
-        (when t
-          (let [p (ray/at ray t)
-                outward-normal (vec3/unit (vec3/sub p center))
-                hr (h/hit-record t p outward-normal false (:material sphere))]
-            (h/set-face-normal hr ray outward-normal)))))))
+         (when (>= discriminant 0.0)
+           (let [sqrtd (math/sqrt discriminant)
+
+                 root1 (/ (- h sqrtd) a)
+                 root2 (/ (+ h sqrtd) a)
+
+                 t (cond
+                     (and (> root1 tmin) (< root1 tmax)) root1
+                     (and (> root2 tmin) (< root2 tmax)) root2
+                     :else nil)]
+
+             (when t
+               (let [p (ray/at ray t)
+                     outward-normal (vec3/unit (vec3/sub p center))
+                     hr (HitRecord. t p outward-normal false material)]
+                 (h/set-face-normal hr ray outward-normal))))))))
 
 (defn create [center radius material]
-  {:type :sphere
-   :center center
-   :radius radius
-   :material material
-   :hit hit})
+  (Sphere. center (double radius) material))
