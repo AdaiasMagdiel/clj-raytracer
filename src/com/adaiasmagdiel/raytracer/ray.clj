1a06043f68127b45ce1c400b879bee234f2f6461
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-14 13:57:39 -0300

    feat: enhance ray creation and calculation performance - Introduce a Ray record for better performance - Use type hints for origin and direction - Improve at function with direct field access - Add set! directives for reflection and unchecked math warnings

--- src/com/adaiasmagdiel/raytracer/ray.clj
+++ src/com/adaiasmagdiel/raytracer/ray.clj
@@ -1,11 +1,20 @@
 (ns com.adaiasmagdiel.raytracer.ray
-	(:require [com.adaiasmagdiel.raytracer.vec3 :as vec3]))
+  (:require [com.adaiasmagdiel.raytracer.vec3 :as vec3])
+  (:import [com.adaiasmagdiel.raytracer.vec3 Vec3]))
 
-(defn create [origin direction]
-	{:origin origin
-		:direction direction})
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
 
-(defn at [ray t]
-	(vec3/add (:origin ray)
- 	(vec3/scalar-mul (:direction ray) t)))
+(defrecord Ray [^Vec3 origin ^Vec3 direction])
 
+(defn create
+  "Creates a high-performance Ray record."
+  ^Ray [^Vec3 origin ^Vec3 direction]
+  (->Ray origin direction))
+
+(defn at
+  "Calculates point at distance t: P(t) = A + tB"
+  ^Vec3 [^Ray ray ^double t]
+  (let [orig (.origin ray)
+        dir  (.direction ray)]
+    (vec3/add orig (vec3/scalar-mul dir t))))
