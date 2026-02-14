ffb11d5dbff93f38c7f11aee32c31b221d5c616f
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-14 13:57:17 -0300

    refactor: improve performance and readability of ray tracing engine - optimize color calculations using cached constants - improve type hints and primitive operations for better performance - enhance code readability by using consistent naming conventions and formatting - update functions to use unchecked math operations for better performance

--- src/com/adaiasmagdiel/engine.clj
+++ src/com/adaiasmagdiel/engine.clj
@@ -6,75 +6,97 @@
             [com.adaiasmagdiel.raytracer.hittable :as h]
             [com.adaiasmagdiel.utils :as utils]
             [com.adaiasmagdiel.raytracer.material :as mat]
-            [clojure.math :as math]))
+            [clojure.math :as math])
+  (:import [com.adaiasmagdiel.raytracer.vec3 Vec3]
+           [com.adaiasmagdiel.raytracer.ray Ray]
+           [com.adaiasmagdiel.raytracer.hittable HitRecord IHittable]
+           [java.util.concurrent ThreadLocalRandom]))
 
-(def ^:const world [(sphere/create (vec3/create  0.0 -100.5 -1.0) 100 (mat/->Lambertian [0.8 0.8 0]))
-                    (sphere/create (vec3/create  0.0 0.0 -1.2)    0.5 (mat/->Lambertian [0.1 0.2 0.5]))
-                    (sphere/create (vec3/create -1.0 0.0 -1.0)    0.5 (mat/->Dielectric 1.50))
-                    (sphere/create (vec3/create -1.0 0.0 -1.0)    0.4 (mat/->Dielectric (/ 1.0 1.50)))
-                    (sphere/create (vec3/create  1.0 0.0 -1.0)    0.5 (mat/->Metal [0.8 0.6 0.2] 1.0))])
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
 
-(defn paint-sky [r]
-  (let [unit-direction (vec3/unit (:direction r))
+;; Cache de cores constantes para evitar chamadas de função
+(def ^:const COLOR-BLACK (vec3/create 0 0 0))
+(def ^:const COLOR-WHITE (vec3/create 1 1 1))
+(def ^:const COLOR-SKY   (vec3/create 0.5 0.7 1.0))
+
+(def world
+  (into-array IHittable
+              [(sphere/create (vec3/create  0.0 -100.5 -1.0) 100 (mat/->Lambertian (vec3/create 0.8 0.8 0)))
+               (sphere/create (vec3/create  0.0 0.0 -1.2)    0.5 (mat/->Lambertian (vec3/create 0.1 0.2 0.5)))
+               (sphere/create (vec3/create -1.0 0.0 -1.0)    0.5 (mat/->Dielectric 1.50))
+               (sphere/create (vec3/create -1.0 0.0 -1.0)    0.4 (mat/->Dielectric (/ 1.0 1.50)))
+               (sphere/create (vec3/create  1.0 0.0 -1.0)    0.5 (mat/->Metal (vec3/create 0.8 0.6 0.2) 1.0))]))
+
+(defn paint-sky [^Ray r]
+  (let [^Vec3 dir (.direction r)
+        unit-direction (vec3/unit dir)
         a (* 0.5 (+ 1.0 (vec3/y unit-direction)))]
     (vec3/add
-     (vec3/scalar-mul (vec3/create 1.0 1.0 1.0) (- 1.0 a))
-     (vec3/scalar-mul (vec3/create 0.5 0.7 1.0) a))))
+     (vec3/scalar-mul COLOR-WHITE (- 1.0 a))
+     (vec3/scalar-mul COLOR-SKY a))))
 
 (defn ray-color
-  ([ray world] (ray-color ray world s/max-depth))
-  ([ray world depth]
+  ([^Ray ray world] (ray-color ray world s/max-depth))
+  ([^Ray ray world ^long depth]
    (if (<= depth 0)
-     [0 0 0]
+     (vec3/create 0 0 0)
 
      (if-let [rec (h/hit-world world ray 0.001 Double/POSITIVE_INFINITY)]
-       (let [material (:material rec)]
+       (let [material (.material ^HitRecord rec)]
          (if-let [scatter-result (mat/scatter material ray rec)]
            
            (let [{:keys [scattered attenuation]} scatter-result]
-             (vec3/mul attenuation (ray-color scattered world (dec depth))))
+             (vec3/mul attenuation (ray-color scattered world (unchecked-dec depth))))
            
-           [0 0 0]))
+           (vec3/create 0 0 0)))
 
        (paint-sky ray)))))
 
-(defn linear-to-gama [linear-component]
-  (if (> linear-component 0)
+(defn linear-to-gama ^double [^double linear-component]
+  (if (> linear-component 0.0)
     (math/sqrt linear-component)
-    0))
+    0.0))
 
 (defn write-color [color]
-  (->> color
-       (map linear-to-gama)
-       (map #(utils/clamp % 0.0 0.999))
-       (map #(* 256 %))
-       (map int)))
+  (let [^Vec3 c color
+        cx (.x c)
+        cy (.y c)
+        cz (.z c)
+
+        r (int (* 256.0 (utils/clamp-double (linear-to-gama cx) 0.0 0.999)))
+        g (int (* 256.0 (utils/clamp-double (linear-to-gama cy) 0.0 0.999)))
+        b (int (* 256.0 (utils/clamp-double (linear-to-gama cz) 0.0 0.999)))]
+    [r g b]))
 
 (defn defocus-disk-sample []
   (let [p (vec3/random-in-unit-disk)]
     (vec3/add
      s/camera-center (vec3/scalar-mul s/defocus-disk-u (vec3/x p)) (vec3/scalar-mul s/defocus-disk-v (vec3/y p)))))
 
-(defn get-ray [i j]
-  (let [u (+ i (rand))
-        v (+ j (rand))
+(defn get-ray [^long i ^long j]
+  (let [rng (ThreadLocalRandom/current)
+        u (+ (double i) (.nextDouble rng))
+        v (+ (double j) (.nextDouble rng))
         pixel-center
         (vec3/add (vec3/scalar-mul s/pixel-delta-v v)
                   (vec3/add s/pixel00-loc
                             (vec3/scalar-mul s/pixel-delta-u u)))
-        origin (if (<= s/defocus-angle 0) s/camera-center (defocus-disk-sample)) 
+        origin (if (<= s/defocus-angle 0)
+                 s/camera-center
+                 (defocus-disk-sample))
         direction (vec3/sub pixel-center origin)]
     (ray/create origin direction)))
 
-(defn pixel-color [i j]
-  (let [accumulated
-        (reduce
-         (fn [color _]
-           (let [r (get-ray i j)]
-             (vec3/add color (ray-color r world))))
-         (vec3/create 0 0 0)
-         (range s/samples-per-pixel))]
-    (vec3/scalar-mul accumulated s/pixel-samples-scale)))
+
+(defn pixel-color [^long i ^long j]
+  (loop [n 0
+         acc (vec3/create 0 0 0)]
+    (if (< n s/samples-per-pixel)
+      (let [r (get-ray i j)]
+        (recur (unchecked-inc n)
+               (vec3/add acc (ray-color r world))))
+      (vec3/scalar-mul acc s/pixel-samples-scale))))
 
 (defn compute-pixel-color [x y]
   (write-color (pixel-color x y)))
